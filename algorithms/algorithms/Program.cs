f4b4ed847d7912004610c5063fe92dd95028d4f2
Misha <19fishka96@mail.ru>
2021-12-03 08:18:18 +0300

    create

---
+++ algorithms/algorithms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace algorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d = 0;
+            Console.WriteLine("Введите число");
+          int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 2; i < n / 2; i++)
+            {
+                if (n % i == 0)
+                    d++;
+            }
+            if (d == 0) Console.WriteLine("Число простое");
+            else Console.WriteLine("Число не простое");
+        }
+    }
+}
